5263d37d15f8a422501b994e1b391ede2220970a
BKMarcsi <bkolonuszmaria@gmail.com>
2021-11-15 22:50:43 +0100

    js3_003_02

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,7 @@
 const setCookie = (tokenInput) => {
     let plusFifteen = new Date();
     plusFifteen.setTime(plusFifteen.getTime()+15*60*1000);
-    document.cookie = token=${tokenInput}; expires=${plusFifteen.toUTCString()};
+    document.cookie = `token=${tokenInput}; expires=${plusFifteen.toUTCString()}`;
     
 }
 
